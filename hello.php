50b3fc047536bdbe340de6d54aedc5d4918007af
htetkiplepay <htet.oo@kiplepay.com>
2023-02-16 18:50:24 +0630

    merge 1

--- hello.php
+++ hello.php
@@ -6,3 +6,4 @@ echo "cherry pick fourth commit";
 echo "cherry pick fith commit";
 echo "cherry pick sixth commit";
 echo "cherry pick seven commit";
+merge 1;
--- new_2.php
+++ new_2.php
@@ -1,3 +1,4 @@
 new 2
 new 2 commit 1
 new 2 commit 2
+merge 1
