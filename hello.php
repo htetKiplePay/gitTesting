57db4aee762a122862f027c91f8d3ddecfd4ff30
htetkiplepay <htet.oo@kiplepay.com>
2023-02-18 22:08:42 +0630

    Revert "merge blank"
    This reverts commit baf3cc79319b3dcbcbe469e75f4280c06f074083.

--- hello.php
+++ hello.php
@@ -2,10 +2,12 @@
 echo 'hello world';
 echo "second commit";
 echo "third commit";
-echo "cherry pick fourth commit";
+echo "cherry pick fourth hi commit";
 echo "cherry pick fith commit";
-echo "cherry pick sixth commit";
-echo "cherry pick seven commit";
 merge 1;
 merge 2;
-revert 1;
+hi 1,2;
+hi 1,2;
+
+
+
